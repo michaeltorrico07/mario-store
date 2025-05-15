a43a5badd452807c49601d931e9bcf8814d95590
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-05-15 19:54:59 -0300

    iniciacion del proyecto en expo

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex flex-1 justify-center items-center"
+    >
+      <Text>Welcome.</Text>
+    </View>
+  );
+}
